4f86d78f28e570217671b0cb9fa00f63dc493cdc
LauraAmancha <grimanezaamancha@gmail.com>
2026-02-02 20:03:22 -0500

    Subiendo proyecto SistemasInventarios completo

---
+++ InventarioContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemasInventarios.Models;
+
+namespace SistemasInventarios.Data
+{
+    public class InventarioContext : DbContext
+    {
+        public InventarioContext(DbContextOptions<InventarioContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<OrdenCompra> OrdenesCompras { get; set; }
+    }
+}
---
+++ InventarioContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace SistemasInventarios.Data
+{
+    public class InventarioContextFactory : IDesignTimeDbContextFactory<InventarioContext>
+    {
+        public InventarioContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<InventarioContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SistemasInventariosDB;Trusted_Connection=True;");
+
+            return new InventarioContext(optionsBuilder.Options);
+        }
+    }
+}
